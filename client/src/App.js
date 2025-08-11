5ab495dc873ce241933ec5286f95ca7b29dd9f20
jzach22 <84591601+jzach22@users.noreply.github.com>
2025-08-11 18:20:36 -0400

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/App.js 	modified:   client/src/index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
